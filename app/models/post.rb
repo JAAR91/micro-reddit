c4e7a7825c7a8bf173138c02a932812e4ae1355d
Amalcxc <amalcxirsi@gmail.com>
2021-04-28 23:05:24 +0300

    linter files

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_many :comment
-  
+
   validates :title, presence: true, uniqueness: true
   validates :body, presence: true
   validates :user_id, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_many :post
   has_many :comment
-    
-  validates :username, presence: true, uniqueness: true, length: {in:4..12}
+
+  validates :username, presence: true, uniqueness: true, length: { in: 4..12 }
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: {in:6..16}
+  validates :password, presence: true, length: { in: 6..16 }
 end
