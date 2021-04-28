fe83b6461d8759fd3374df7912bf70d484cbe22a
JAAR96 <jaarriagaramos@gmail.com>
2021-04-28 14:00:06 -0600

    create table coment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :post
+  has_many :comment
     
   validates :username, presence: true, uniqueness: true, length: {in:4..12}
   validates :email, presence: true, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_28_185210) do
+ActiveRecord::Schema.define(version: 2021_04_28_195509) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
